8174d5053a79c4f1b95ccebe6e88b01313b61cf1
Winzar <connorwinzar@gmail.com>
2022-04-06 18:23:47 +0100

    calculator cypress commit

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,40 @@
+describe("Check: Does number button print to display", () => {
+    it("Can print a single number to display", () => {
+        cy.visit("http://127.0.0.1:5500/")
+
+        // cy.get("[data-cy=nine]").click();
+        cy.get(".calculator__number-nine").click();
+        cy.get(".calculator__number-nine").click();
+        //cy.get(".calculator__display-value").should("have.text", "9");
+        cy.get(".calculator__display").should("have.text", "99");
+        // cy.get(".calculator__display").should(($div) => {
+        //     expect($div.text().trim()).equal("99");
+        //   });
+          
+
+
+    })
+
+})
+
+
+
+
+// describe("Check: Does operator button print to display", () => {
+//     it("Does not do much", () => {
+
+
+// describe("Check: Can it perform a simple operation", () => {
+//     it("Does not do much", () => {
+//         expect(true).to.equal(true);
+//     })
+// })
+
+
+
+// Initial code
+// describe("Check: Can it check", () => {
+//     it("Does not do much", () => {
+//         expect(true).to.equal(true);
+//     })
+// })
